541856959e9485db3dc5703802ed02ad9942f0d8
JuadaDev <juadadev@gmail.com>
2026-05-20 19:07:29 -0500

    replacing numeric validation with replacing with blank spaces

--- 5_the_countdown_to_take_off.py
+++ 5_the_countdown_to_take_off.py
@@ -5,22 +5,17 @@ takeoff = "2025*12*25@00|00|00 NP"
 
 
 def time_until_take_off(from_time: str, take_off_time: str) -> int:
-    return 0
+    def clear_time(time: str) -> datetime:
+        for delim in ["*", "@", "|"]:
+            time = time.replace(delim, " ")
+        time = time.replace("NP", "")
+
+        return datetime.strptime(time.replace(" ", ""), "%Y%m%d%H%M%S")
+
+    list_from_time = clear_time(from_time)
+    take_off_time_converted = clear_time(take_off_time)
+    diff_seconds = (take_off_time_converted - list_from_time).total_seconds()
+    return int(diff_seconds)
 
 
-print(datetime.today())
-
-
-list_from_time = [char for char in from_time if char.isnumeric()]
-str_date = "".join(list_from_time)
-print(str_date)
-list_takeoff = [char for char in takeoff if char.isnumeric()]
-str_date2 = "".join(list_takeoff)
-print(str_date2)
-
-convert_date = datetime.strptime(str_date, "%Y%m%d%H%M%S")
-convert_date2 = datetime.strptime(str_date2, "%Y%m%d%H%M%S")
-print(convert_date)
-print(convert_date2)
-seconds = (convert_date2 - convert_date).total_seconds()
-print(int(seconds))
+print(time_until_take_off(from_time, takeoff))
